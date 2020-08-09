8f0fb2026783206b57dfd8e8f80f1854f6a6f57f
Hadi Tedi <haditedi@gmail.com>
2020-08-09 19:59:06 +0100

    success - tested form sending

--- src/components/formui.js
+++ src/components/formui.js
@@ -4,27 +4,25 @@ import classes from "./formui.module.css"
 import Button from "@material-ui/core/Button"
 import SendIcon from "@material-ui/icons/Send"
 
-const formui = () => {
+const formui = ({ change, submit, state }) => {
   return (
-    <form
-      className={classes.formui}
-      name="contact"
-      action="https://us-central1-firestore-b0c8c.cloudfunctions.net/emailMessage"
-      method="POST"
-    >
-      <input type="hidden" name="form-name" value="contact"></input>
+    <form className={classes.formui} name="contact" method="POST">
       <TextField
         required
         id="outlined-required"
         label="Name"
         name="name"
         variant="outlined"
+        onChange={change}
+        value={state.name}
       />
       <TextField
         id="outlined-number"
         label="Phone Number"
         name="number"
         variant="outlined"
+        onChange={change}
+        value={state.number}
       />
       <TextField
         id="email"
@@ -32,6 +30,8 @@ const formui = () => {
         type="email"
         name="email"
         variant="outlined"
+        onChange={change}
+        value={state.email}
       />
       <TextField
         id="message"
@@ -40,6 +40,8 @@ const formui = () => {
         multiline
         rows={4}
         variant="outlined"
+        onChange={change}
+        value={state.message}
       />
       <Button
         style={{ margin: "10px" }}
@@ -47,6 +49,7 @@ const formui = () => {
         color="primary"
         type="submit"
         endIcon={<SendIcon />}
+        onClick={submit}
       >
         SEND
       </Button>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import HeadingText from "../components/HeadingText"
 import Formui from "../components/formui"
+import axios from "axios"
 
-const contact = () => {
+const Contact = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    message: "",
+    number: "",
+  })
+
+  const handleChange = e => {
+    const { name, value } = e.target
+    console.log(name, value)
+    setState(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value,
+      }
+    })
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios
+      .post("https://awe-ads.herokuapp.com/send-email", state)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
   return (
     <Layout>
       <div>
         <HeadingText>Contact Us</HeadingText>
       </div>
       <div>
-        <Formui />
+        <Formui change={handleChange} submit={handleSubmit} state={state} />
       </div>
     </Layout>
   )
 }
 
-export default contact
+export default Contact
